349b2eba17333d7f9b22399a9a6018a4b88d6519
dabhivaibhav <vaibhavdabhi063@gmail.com>
2025-06-25 22:56:52 -0500

    implemented new logic pattern questions

--- logicbuildingpattern/Pattern15.java
+++ logicbuildingpattern/Pattern15.java
@@ -4,23 +4,29 @@ import java.util.Scanner;
 
 /**
  * 1
- * 2 3
- * 4 5 6
- * 7 8 9 10
- * 11 12 13 14 15
+ * 23
+ * 456
+ * 7890
+ * 12345
+ * 678901
+ * 2345678
  */
-public class Pattern14 {
+public class Pattern15 {
     public static void main(String[] args) {
-        System.out.println("Enter the number of rows: ");
+        System.out.print("Enter the number of row: ");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int num = 1;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
+                if (num > 9) {
+                    num = 0;
+                }
                 System.out.print(num + " ");
                 num++;
             }
             System.out.println();
         }
+
     }
 }
