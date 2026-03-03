b0a5f70e530e285e93de3b0c3b27ffc9c9ccdff5
Vaibhav Dabhi <vaibhavdabhi063@gmail.com>
2026-03-03 09:45:35 -0600

    implemented min heap and max heap using priority queue

---
+++ heap/HeapImplPriorityQueue.java
@@ -0,0 +1,46 @@
+package heap;
+
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class HeapImplPriorityQueue {
+
+    public static void main(String[] args) {
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        minHeap.add(10);
+        minHeap.add(5);
+        minHeap.add(15);
+        minHeap.add(20);
+        minHeap.add(1);
+        minHeap.add(12);
+
+        maxHeap.add(10);
+        maxHeap.add(5);
+        maxHeap.add(15);
+        maxHeap.add(20);
+        maxHeap.add(1);
+        maxHeap.add(12);
+
+        System.out.println("Min Heap: " + minHeap);
+        System.out.println("Max Heap: " + maxHeap);
+
+        System.out.println();
+
+        System.out.println("Removed element from min heap: " + minHeap.poll());
+        System.out.println("Removed element from max heap: " + maxHeap.poll());
+
+        System.out.println();
+
+        System.out.println("Min Heap: " + minHeap);
+        System.out.println("Max Heap: " + maxHeap);
+
+        System.out.println();
+
+        System.out.println("Top element of min heap: " + minHeap.peek());
+        System.out.println("Top element of max heap: " + maxHeap.peek());
+    }
+}
