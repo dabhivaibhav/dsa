1aa51407fa8d32972f7e15b81dd3b6bb7b415a26
Vaibhav Dabhi <vaibhavdabhi063@gmail.com>
2026-03-04 18:25:40 -0600

    Refactore the code to find top k element both largest and smallest using heap

--- heap/FindKSmallestElement.java
+++ heap/FindKSmallestElement.java
@@ -3,13 +3,13 @@ package heap;
 import java.util.Arrays;
 
 /*
-Problem: K Largest element in an array
+Problem: K Smallest element in an array
 
 Given an array nums, return the kth largest element in the array.
 
 Example 1
 Input: nums = [1, 2, 3, 4, 5], k = 2
-Output: 4
+Output: 2
 
 Example 2
 Input: nums = [-5, 4, 1, 2, -3], k = 5
@@ -21,31 +21,33 @@ Constraints:
             1 <= k <= nums.length
 
  */
-public class FindKLargestElement {
+public class FindKSmallestElement {
 
     public static void main(String[] args) {
-        int[] nums = {3, 4, 2, 1, 5};
-        int k = 2;
-        System.out.println(Arrays.toString(findKLargestElement(nums, k)));
+        int[] nums = {3, 10, 1, 2, 5};
+        int k = 3;
+        System.out.println(Arrays.toString(findKSmallestElement(nums, k)));
     }
 
-    private static int[] findKLargestElement(int[] nums, int k) {
-        MinHeap heap = new MinHeap(); // Your class!
+
+    public static int[] findKSmallestElement(int[] nums, int k) {
+        MaxHeap heap = new MaxHeap();
 
         for (int num : nums) {
             heap.insert(num);
 
             // If the heap gets too big, kick out the smallest person
             if (heap.getSize() > k) {
-                heap.delete();
+                heap.pop();
             }
         }
 
-        // The root of the Min-Heap is the K largest
+        // The root of the Max-Heap is the K largest
         int[] heapArray = new int[k];
         for (int i = 0; i < k; i++) {
-            heapArray[i] = heap.delete();
+            heapArray[i] = heap.pop();
         }
+
         return heapArray;
     }
 }
